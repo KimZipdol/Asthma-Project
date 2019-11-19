ecbd33c545fb039a2ba1449c2645e6f168a90e26
JimoonKim <wlansdl@gmail.com>
2019-11-19 22:31:34 +0900

    UI, 로켓위치

---
+++ Assets/2. Script/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    FIRST_SCREEN,
+    TUTORIAL,
+    PLAYING,
+    ROCKET_LAUNCH,
+    GAME_RESULT,
+    RANKING
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        
+    }
+}
---
+++ Assets/2. Script/RocketBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketBehavior : MonoBehaviour
+{
+    [SerializePrivateVariables]
+    private float fev1 = 0f;
+    private float fvc = 0f;
+    private float ratio = 0f; 
+
+    public void calculate()
+    {
+        ratio = fev1 / fvc;
+    }
+
+
+}
