47c5f231ef09152ed05f4b3a0a3fb7dfa73ba388
KimZipdol <wlansdl@gmail.com>
2020-03-30 23:32:52 +0900

    200330

--- Assets/2. Script/Rocket/GameManager.cs
+++ Assets/2. Script/Rocket/GameManager.cs
@@ -1,19 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO.Ports;
 
-public enum GameState
-{
-    FIRST_SCREEN,
-    TUTORIAL,
-    PLAYING,
-    ROCKET_LAUNCH,
-    GAME_RESULT,
-    RANKING
-}
 
 public class GameManager : MonoBehaviour
 {
+    SerialPort sp = new SerialPort("COM3", 115200);
+
+
     public static GameManager instance = null;
 
     private void Awake()
@@ -31,6 +26,29 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        
+        sp.Open();
+        sp.ReadTimeout = 1;
+    }
+
+    void Update()
+    {
+        if(sp.IsOpen)
+        {
+            try
+            {
+                /*float pressureVal = sp.Read();
+                Debug.Log(pressureVal.ToString());
+                */
+
+                string value = sp.ReadLine();
+                print(value);
+                sp.BaseStream.Flush();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
     }
 }
