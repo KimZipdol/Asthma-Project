31287c2be4525e1d9caa955998d5853a799d958b
KimZipdol <wlansdl@gmail.com>
2020-05-25 00:28:23 +0900

    IntroScene

---
+++ Assets/2. Script/InitialScene/PlayerBehavior.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public Transform tr = null;
+
+    private LineRenderer line;
+    private int effectTurn = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        line = GetComponent<LineRenderer>();
+        line.enabled = false;
+        line.startWidth = 0.1f;
+        line.endWidth = 0.05f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(tr.position, tr.forward);
+        RaycastHit hit;
+
+        line.SetPosition(0, tr.position);
+        if (Physics.Raycast(tr.position, tr.forward, out hit, 100f))
+        {
+            line.SetPosition(1, hit.point);
+            if (hit.collider.gameObject.CompareTag("INTERACTABLE"))
+            {
+                hit.collider.gameObject.GetComponent<Outline>().SetActive(true);
+            }
+        }
+        else line.SetPosition(1, ray.GetPoint(100.0f));
+
+        StartCoroutine(this.ShowLaserBeam());
+    }
+
+    IEnumerator ShowLaserBeam()
+    {
+        line.enabled = true;
+        yield return new WaitForSeconds(Random.Range(0.01f, 0.2f));
+        line.enabled = false;
+    }
+}
---
+++ Assets/2. Script/Rocket/BackgroundFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundFollow : MonoBehaviour
+{
+    public Transform rocketTr;
+    public List<Transform> backgrounds = new List<Transform>();
+
+    int turn = 0;
+    float currY = 0f;
+    Vector3 currPos;
+    Vector3 newPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rocketTr.position.y>=currY + 700f)
+        {
+            currPos = backgrounds[turn].position;
+            newPos = new Vector3(currPos.x, currPos.y + 1400f, currPos.z); 
+
+            backgrounds[turn].position = newPos;
+            turn = (turn + 1) % 2;
+            currY += 1400f;
+        }
+    }
+}
