d33ad8ce2b96114282ba03b12b5f5e9986c334a1
JimoonKim <wlansdl@gmail.com>
2020-05-25 15:58:30 +0900

    초기씬 완선

---
+++ Assets/2. Script/InitialScene/ButtonCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonCtrl : MonoBehaviour
+{
+    public GameObject player;
+    public Outline thisOutLine;
+
+
+    public void OnRayHit()
+    {
+        thisOutLine.enabled = true;
+        player.SendMessage("StartSelection");
+    }
+
+    public void OutLineOff()
+    {
+        thisOutLine.enabled = false;
+    }
+}
--- Assets/2. Script/InitialScene/PlayerBehavior.cs
+++ Assets/2. Script/InitialScene/PlayerBehavior.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerBehavior : MonoBehaviour
 {
     public Transform tr = null;
+    public GameObject SelectBG = null;
+    public Image SelectImg = null;
+    public float SelectionTime = 2f;
+
+    [SerializeField]
+    private float fps = 60f;
 
     private LineRenderer line;
-    private int effectTurn = 0;
+
+    Ray ray = new Ray();
+    private RaycastHit hit;
 
 
     // Start is called before the first frame update
@@ -19,26 +29,26 @@ public class PlayerBehavior : MonoBehaviour
         line.startWidth = 0.1f;
         line.endWidth = 0.05f;
 
+        StartCoroutine(this.ShowLaserBeam());
+        ray = new Ray(tr.position, tr.forward);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Ray ray = new Ray(tr.position, tr.forward);
-        RaycastHit hit;
+        
 
         line.SetPosition(0, tr.position);
         if (Physics.Raycast(tr.position, tr.forward, out hit, 100f))
         {
             line.SetPosition(1, hit.point);
-            if (hit.collider.gameObject.CompareTag("INTERACTABLE"))
+            
+            if (hit.collider.gameObject.layer==8)
             {
-                hit.collider.gameObject.GetComponent<Outline>().SetActive(true);
+                hit.collider.gameObject.SendMessage("OnRayHit");
             }
         }
         else line.SetPosition(1, ray.GetPoint(100.0f));
-
-        StartCoroutine(this.ShowLaserBeam());
     }
 
     IEnumerator ShowLaserBeam()
@@ -46,5 +56,44 @@ public class PlayerBehavior : MonoBehaviour
         line.enabled = true;
         yield return new WaitForSeconds(Random.Range(0.01f, 0.2f));
         line.enabled = false;
+    }
+
+    public void StartSelection()
+    {
+        StartCoroutine(SelectGame());
+    }
+
+    IEnumerator SelectGame()
+    {
+        SelectBG.SetActive(true);
+        while(hit.collider.gameObject.layer == 8)
+        {
+            if(SelectImg.fillAmount>=0.99)
+            {
+                switch (hit.collider.gameObject.tag)
+                {
+                    case ("ROCKETGAME"):
+                        SceneManager.LoadScene("1. RocketGame", LoadSceneMode.Single);
+                        break;
+                    case ("CANDLEGAME"):
+                        SceneManager.LoadScene("2. CandleBlowing", LoadSceneMode.Single);
+                        break;
+                    case ("FOODGAME"):
+                        SceneManager.LoadScene("3. Inhaler", LoadSceneMode.Single);
+                        break;
+                    case ("ENDGAME"):
+                        Application.Quit();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            SelectImg.fillAmount += 1 / (fps * SelectionTime);
+            yield return Time.deltaTime;
+        }
+        hit.collider.gameObject.SendMessage("OutLineOff");
+        SelectBG.SetActive(false);
+        SelectImg.fillAmount = 0f;
+        
     }
 }
